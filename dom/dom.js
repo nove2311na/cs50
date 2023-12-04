78a4f403939b6abbb3681c91a7960ae9c9fca3c3
Ngo Anh Quan <quanna@hivetech.vn>
2023-12-04 10:27:51 +0700

    message

---
+++ dom/dom.js
@@ -0,0 +1,21 @@
+// 1. DOM Node
+// DOM - Document Object Model
+// DOM Node & DOM Element
+// DOM Attribute & DOM Property
+
+// 2. Selecting DOM Nodes
+// 2.1. document.querySelector(): .header, p, #heading
+const singleNode = document.querySelector("h1");
+console.log(singleNode);
+const singleNode2 = document.querySelector(".container");
+console.log(singleNode2);
+const singleNode3 = document.querySelector("#spinner");
+console.log(singleNode3);
+const singleNode4 = document.querySelector("#heading");
+console.log(singleNode4);
+
+// 2.2. document.querySelectorAll()
+// 2.3. document.getElementById()
+// 2.4. document.getElementsByClassName()
+// 2.5. document.getElementsByTagName()
+// 2.6. document.getElementsByName()
